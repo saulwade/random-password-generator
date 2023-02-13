534a7b18e7870ba5add8c367aa8c40ecfd0ed5f4
saulwade <saulwade29@gmail.com>
2023-02-13 14:36:42 -0600

    added variables to js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,24 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Variables
+var specialChar = ["!", "'", "#", "$", "%", "&", "(", ")", ",", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var upperC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];//array
+var lowerC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //array
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];//array
+
+function writePassword() {
+  var password = generatePassword();//given
+  var passwordText = document.querySelector("#password");//given
+  if (password) {
+    passwordText.value = password;
+  }
+};
+
+
+
+
